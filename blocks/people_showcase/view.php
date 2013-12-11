d377a9ff18e093f8be94d1a7c4372f8fc4383f34
Hans-Albert Maritz <maritz.hans@gmail.com>
2013-12-12 04:02:47 +1100

    Worked on showcase block view, mostly works now

--- blocks/people_showcase/view.php
+++ blocks/people_showcase/view.php
@@ -1,2 +1,99 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")) ?>
-<?php echo $content ?>
+
+<div id="Slide">
+	<ul>
+
+<?php
+$db = Loader::db();
+$results = $db->query('SELECT * from btPersons');
+$rows = $results->GetArray();
+
+#Get all the files in the people fileset
+Loader::model("file_list");
+Loader::model("file_set");
+$fs = FileSet::getByName('people');
+$fl = new FileList();
+$fl->filterBySet($fs);
+$files = $fl->get();
+
+foreach( $rows as $row) {
+	$found = false;
+	foreach($files as $file) {
+		if($file->getFileName() === $row['image']) {
+			$imageFile = $file;
+			$found = true;
+		}
+	}
+	if(found && $imageFile) {
+?>
+		<li>
+			<img src="<?php echo $imageFile->getDownloadUrl();?>">
+		</li>
+
+<?php
+	}
+}
+?>
+
+	</ul>
+	<!--Controls-->
+	<div id=Left>
+		<div class="glyphicon glyphicon-chevron-left"></div>
+	</div>
+	<div id=Right>
+		<div class="glyphicon glyphicon-chevron-right"></div>
+	</div>
+</div>
+
+<script>
+var scrollPos = 0;
+
+$("#Slide li").each(function (index) {
+	console.log(index + ": " + $(this).text());
+	$(this).css({
+		"left": index * 150 - scrollPos
+	});
+});
+
+
+$("#Left").click(function () {
+	scrollPos -= 150;
+	$("#Slide li").each(function (index) {
+		console.log(scrollPos);
+		$(this).animate({
+			"left": index * 150 - scrollPos
+		}, 650);
+	});
+});
+
+$("#Right").click(function () {
+	scrollPos += 150;
+	$("#Slide li").each(function (index) {
+		console.log(scrollPos);
+		$(this).animate({
+			"left": index * 150 - scrollPos
+		}, 650);
+	});
+});
+
+$("#Slide img").hover(function() {
+	$(this).stop(true, false).animate({ "width" : "300px"});
+	$(this).parent().stop(true, false).animate({"left" : ($(this).parent().index() * 150 - scrollPos - 25) + "px"});
+
+}, function() {
+	$(this).stop(true, false).animate({ "width" : "250px"});
+	$(this).parent().stop(true, false).animate({"left" : ($(this).parent().index() * 150 - scrollPos) + "px"});
+});
+
+$("#Slide #Left").hover(function(){
+	$("#Slide #Left .glyphicon").stop(true, false).animate({ "font-size" : "28px", "top" : "44%" });
+}, function() {
+	$("#Slide #Left .glyphicon").stop(true, false).animate({ "font-size" : "24px", "top": "45%"});
+});
+
+$("#Slide #Right").hover(function(){
+	$("#Slide #Right .glyphicon").stop(true, false).animate({ "font-size" : "28px" });
+}, function() {
+	$("#Slide #Right .glyphicon").stop(true, false).animate({ "font-size" : "24px" });
+});
+</script>
--- themes/lawler/people.php
+++ themes/lawler/people.php
@@ -2,44 +2,23 @@
 <link rel="stylesheet" type="text/css" href="<?=$this->getThemePath()?>/css/peoples.css">
 <div id="content">
 	<div id="container">
-		<div id="highlights"> 
-			<a href="chris-davis"><img src="<?=$this->getThemePath()?>/images/people/People_Group_Photo.jpg"></a>
-		</div>
-		<div class="scrollright" style="float: right; cursor: pointer; margin-bottom: 5px; margin-right: 5px;">RIGHT</div>
-		<div class="scrollleft" style="float: left; cursor: pointer; margin-bottom: 5px; margin-right: 5px;">LEFT</div>
-		<script>
-$('.scrollleft').click(function () {
-		console.log('ok')
-		$('#highlights').animate({
-scrollLeft: '-=153'
-}, 500, 'easeOutQuad');
-		});
-$('.scrollright').click(function () {
-		console.log('ok')
-		$('#highlights').animate({
-scrollLeft: '+=153'
-}, 500, 'easeOutQuad');
-		});
-		</script>
+			<?php $b = new Area('Showcase'); $b->display($c); ?>
 
 		<div id="welcome"> 
-			<p>With offices in Newcastle and Tamworth...
-			</p>
-			</p>Our experienced team...</p>
-		</div>
-	</div>
-</div><!-- container -->
-</div><!-- content -->
-<?php Loader::element('footer_required'); ?>
-<!-- include Cycle plugin -->
-<script src="http://malsup.github.com/jquery.cycle2.js"></script>
-<script src="<?=$this->getThemePath()?>/js/bootstrap.js"></script>
-<script>
+			<?php $b = new Area('Introduction'); $b->display($c); ?>
+		</div><!-- welcome -->
+
+	</div><!-- container -->
+</div><!-- conent -->
+	<?php Loader::element('footer_required'); ?>
+	<!-- include Cycle plugin -->
+	<script src="http://malsup.github.com/jquery.cycle2.js"></script>
+	<script src="<?=$this->getThemePath()?>/js/bootstrap.js"></script>
+	<script>
 $("#tabs a").click(function (e) {
 		e.preventDefault();
 		$(this).tab("show");
 		});
-</script>
-</div>
+	</script>
 </body>
 </html>
