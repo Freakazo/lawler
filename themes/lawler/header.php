50f15582f85fb304a81f892f092c25bba12982c2
Hans-Albert Maritz <maritz.hans@gmail.com>
2013-12-12 14:05:56 +1100

    Reverted attempts at getting ie8 to work

--- themes/lawler/header.php
+++ themes/lawler/header.php
@@ -25,6 +25,16 @@
 								<nav class="navbar navbar-default navbar-right" role="navigation">
 										<!-- Brand and toggle get grouped for better mobile display -->
 										<div class="nav-header">
+												<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+														<span class="sr-only">Toggle navigation</span>
+														<span class="icon-bar"></span>
+														<span class="icon-bar"></span>
+														<span class="icon-bar"></span>
+												</button>
+										</div>
+
+										<!-- Collect the nav links, forms, and other content for toggling -->
+										<div class="collapse navbar-collapse col-md4" id="bs-example-navbar-collapse-1">
 												<ul class="nav navbar-nav navbar-right">
 														<?php
 														$bt = BlockType::getByHandle('autonav');
@@ -33,8 +43,7 @@
 														$bt->render('templates/bootstrap_autonav');
 														?>
 												</ul>
-										</div>
-
+										</div><!-- /.navbar-collapse -->
 								</nav>
 						</div> <!--row -->
 				</div>
