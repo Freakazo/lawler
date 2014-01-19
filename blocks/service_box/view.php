58838f34ad95e5508825f7e1aed03985783a20b9
Hans-Albert Maritz <maritz.hans@gmail.com>
2014-01-20 03:30:13 +1100

    Fixed issues with service box

--- blocks/service_box/view.php
+++ blocks/service_box/view.php
@@ -2,30 +2,33 @@
 ?>
 
 <div class="panel panel-default">
-<div class="panel-heading">
-<h4 class="panel-title">
+	<div class="panel-heading">
+		<h4 class="panel-title">
 
-<?php  if (!empty($field_2_textbox_text)): ?>
-	<a href="#<?php  echo htmlentities($field_2_textbox_text, ENT_QUOTES, APP_CHARSET); ?>" data-toggle="collapse" data-parent="#accordion">
-<?php  endif; ?>
+		<?php  if (!empty($field_2_textbox_text)): ?>
+			<strong>
+			<a href="#<?php  echo htmlentities($field_2_textbox_text, ENT_QUOTES, APP_CHARSET); ?>" data-toggle="collapse" data-parent="#accordion">
+		<?php  endif; ?>
 
-<?php  if (!empty($field_3_textbox_text)): ?>
-	<?php  echo htmlentities($field_3_textbox_text, ENT_QUOTES, APP_CHARSET); ?></a></h4>
-</div>
-<?php  endif; ?>
+		<?php  if (!empty($field_3_textbox_text)): ?>
+			<?php  echo htmlentities($field_3_textbox_text, ENT_QUOTES, APP_CHARSET); ?></a>
+			</strong>
+			</h4>
+		</div>
+		<?php  endif; ?>
 
-<?php  if (!empty($field_2_textbox_text)): ?>
-	<div id="<?php  echo htmlentities($field_2_textbox_text, ENT_QUOTES, APP_CHARSET); ?>" class="panel-collapse collapse">
-<?php  endif; ?>
+		<?php  if (!empty($field_2_textbox_text)): ?>
+			<div id="<?php  echo htmlentities($field_2_textbox_text, ENT_QUOTES, APP_CHARSET); ?>" class="panel-collapse collapse">
+		<?php  endif; ?>
 
-<div class="panel-body">
+		<div class="panel-body">
 
-<?php  if (!empty($field_6_wysiwyg_content)): ?>
-	<?php  echo $field_6_wysiwyg_content; ?>
-<?php  endif; ?>
+			<?php  if (!empty($field_6_wysiwyg_content)): ?>
+				<?php  echo $field_6_wysiwyg_content; ?>
+			<?php  endif; ?>
 
-</div>
-</div>
+		</div>
+	</div>
 </div>
 
 <?php  if ($field_8_select_value == 1): ?>
