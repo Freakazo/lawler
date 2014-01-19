b74f02adc4d327a57f64a60e135d1f5572573a93
Hans-Albert Maritz <maritz.hans@gmail.com>
2014-01-20 03:31:20 +1100

    New contact us display

--- themes/lawler/contact.php
+++ themes/lawler/contact.php
@@ -7,28 +7,39 @@
 				<?php $b = new Area('Introduction'); $b->display($c); ?>
 			</div>
 			<?php $b = new Area('Content'); $b->display($c); ?>
+
 			<div class=row>
-				<div class="col-md-6">
-					<div class=row>
-						<div class="col-md-4">
-							<?php $b = new Area('one'); $b->display($c); ?>
-						</div>
-						<div class="col-md-4">
-							<?php $b = new Area('two'); $b->display($c); ?>
-						</div>
-						<div class="col-md-4">
-							<?php $b = new Area('three'); $b->display($c); ?>
-						</div>
-					</div>
+				<div class="col-md-4">
+					<?php $b = new Area('one'); $b->display($c); ?>
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-8">
 					<?php $b = new Area('map'); $b->display($c); ?>
 				</div>
 			</div>
+			<hr>
+
+			<div class=row>
+				<div class="col-md-4">
+					<?php $b = new Area('two'); $b->display($c); ?>
+				</div>
+				<div class="col-md-8">
+					<?php $b = new Area('mapTwo'); $b->display($c); ?>
+				</div>
+			</div>
+			<hr>
+
+			<div class=row>
+				<div class="col-md-4">
+					<?php $b = new Area('three'); $b->display($c); ?>
+				</div>
+				<div class="col-md-8">
+					<?php $b = new Area('mapThre'); $b->display($c); ?>
+				</div>
+			</div>
+			<?php $this->inc('elements/footer.php'); ?>
 		</div>
-		<?php $this->inc('elements/footer.php'); ?>
 	</div>
 </div>
-<?php Loader::element('footer_required'); ?>
+	<?php Loader::element('footer_required'); ?>
 </body>
 </html>
