4bd9f36716bf79aae2ed47cb7ba83b1585a3a42b
Hans-Albert Maritz <maritz.hans@gmail.com>
2014-01-20 03:29:10 +1100

    Autonav now includes all service box blocks in navigation

--- blocks/autonav/templates/bootstrap_autonav.php
+++ blocks/autonav/templates/bootstrap_autonav.php
@@ -26,37 +26,27 @@ $navItems = $controller->getNavItems();
 			$realUrl = $ni->url;
 		}
 	?>	
-	<li class="dropdown <?php echo $classes ?>">
+	<li class="dropdown <?php echo $classes?>">
 		<a href="<?php echo $realUrl?>" class="dropdown-toggle disabled" data-toggle="dropdown">Services<b class="caret"></b></a>
 		<ul class="dropdown-menu">
-<li><a href="<?php echo $realUrl?>#retirement-planning" title="Go to the Retirement Planning page">Retirement Planning</a></li> 
-
-<li><a href="<?php echo $realUrl?>#investment-advice" title="Go to the Investment Advice page">Investment Advice</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#superannuation-wealth-creation-strategies" title="Go to the Superannuation &amp; Wealth Creation Strategies page">Superannuation &amp; Wealth Creation Strategies</a>
-</li> 
-
-<li><a href="<?php echo $realUrl?>#self-managed-super-funds" title="Go to the Self Managed Super Funds page">Self Managed Super Funds</a>
-</li> 
-	
-<li><a href="<?php echo $realUrl?>#tax-effective-planning" title="Go to the Tax Effective Planning page">Tax Effective Planning</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#direct-equities" title="Go to the Direct Equities page">Direct Equities</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#protected-loans" title="Go to the Protected Loans page">Protected Loans</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#insurance" title="Go to the Insurance page">Insurance</a></li> 
-	
-	    
-<li><a href="<?php echo $realUrl?>#estate-planning" title="Go to the Estate Planning page">Estate Planning</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#margin-lending" title="Go to the Margin Lending page">Margin Lending</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#negative-gearing" title="Go to the Negative Gearing page">Negative Gearing</a></li> 
-	
-<li><a href="<?php echo $realUrl?>#instalment-gearing" title="Go to the Instalment Gearing page">Instalment Gearing</a></li>
-</ul>
-</li>
+		<?php
+			$db = Loader::db();
+			$results = $db->query('SELECT * from btDCServiceBox');
+			$rows = $results->GetArray();
+			foreach( $rows as $row) {
+				echo "<li><a href=\"";
+				echo $realUrl;
+				echo "#";
+				echo $row['field_2_textbox_text'];
+				echo "\" title=\"Go to the ";
+				echo $row['field_3_textbox_text'];
+				echo " page\">";
+				echo $row['field_3_textbox_text'];
+				echo "</a></li>";
+			}
+		?>
+		</ul>
+		</li>
 
 <?php
 		continue;
